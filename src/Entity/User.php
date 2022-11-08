be13d6f896ad82a3b3a7b75ad963a5dd406cad71
kewinwarzecha <kewin.warzecha@g-group.dev>
2022-11-08 17:09:13 +0100

    Fixed typo.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -129,7 +129,7 @@ class User extends AbstractEntity implements UserInterface, PasswordAuthenticate
         return $this;
     }
 
-    public function getPlainPassword(): string
+    public function getPlainPassword(): ?string
     {
         return $this->plainPassword;
     }
